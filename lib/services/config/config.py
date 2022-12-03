ece6e97ec00eb6a12b958c490a7e9956d87cefb9
Oscar Band <111942733+gtoscarband@users.noreply.github.com>
2022-12-03 18:32:34 -0500

    Update config.py

--- lib/services/config/config.py
+++ lib/services/config/config.py
@@ -2,9 +2,9 @@ def getConcertInfo():
     CONCERT_INFO = {
         "email_subject": "GT OSCAR BAND - FALL 2022 CONCERT - PAYMENT CONFIRMATION",
 
-        "concert_name": "GT OSCAR BAND FALL 2022 CONCERT",
-        "concert_time": "March 4th 6:00 PM ~ 7:30 PM",
-        "concert_location": "East Architecture Building Room 123 (245 4th St NW)",
+        "concert_name": "GT OSCAR BAND FALL 2022 END OF SEMESTER CONCERT",
+        "concert_time": "December 7th (12/7) 6:00 PM ~ 7:30 PM",
+        "concert_location": "John Lewis Student Center, Atlantic Theatre (351 Ferst Dr NW, Atlanta)",
         "concert_contact": "470-676-6977",
     }
     return CONCERT_INFO
